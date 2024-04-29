50fcbe1030f40430eb1ac66dc4727a41d018e29f
”Kazakov <dkazakoov@gmail.com>
2024-04-29 09:51:15 +0300

    class JsonService

--- WeatherService/JsonService.cpp
+++ WeatherService/JsonService.cpp
@@ -2,19 +2,20 @@
 using nlohmann::json;
 Weather JsonService::getWeather(std::string s)
 {
+    setlocale(LC_ALL, "ru");
     std::ifstream fin(s);
     if (!fin)
         throw std::exception("error");
 
     json j;
     j = json::parse(fin);
-    std::string city = j["name"]; //  Киров
-    double lon = j["coord"]["lon"]; // 49.6601
-    double lat = j["coord"]["lat"]; // 58.5966  
-    double temperature = j["main"]["temp"]; // 5.69
-    std::string weather = j["weather"][0]["description"]; // дождь
-    double windSpeed = j["wind"]["speed"]; // 4.27
-    int clouds = j["clouds"]["all"]; // 100
+    std::string city = j["name"].get<std::string>(); //  Киров
+    double lon = j["coord"]["lon"].get<double>(); // 49.6601
+    double lat = j["coord"]["lat"].get<double>(); // 58.5966  
+    double temperature = j["main"]["temp"].get<double>(); // 5.69
+    std::string weather = j["weather"][0]["description"].get<std::string>(); // дождь
+    double windSpeed = j["wind"]["speed"].get<double>(); // 4.27
+    int clouds = j["clouds"]["all"].get<int>(); // 100
     return Weather(city,lon,lat,temperature,weather,windSpeed,clouds);
 }
 
--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,8 +1,14 @@
 ﻿#include <vector>
 #include "json.hpp"
 #include "pugixml-1.14/src/pugixml.hpp"
+#include "Weather.h"
+#include "Service.h"
+#include "JsonService.h"
 using namespace std;
 
 int main() {
-
+	setlocale(LC_ALL, "ru");
+	JsonService js;
+	Weather w = js.getWeather("weather.json");
+	w.getWeather();
 }
